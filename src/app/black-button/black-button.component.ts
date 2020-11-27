39d38bff8354f0f7f93909caa9877583265ee7eb
dhayananth <dhayanthdharma@gmail.com>
2020-11-27 23:14:49 +0100

    home page design completed

---
+++ src/app/black-button/black-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-black-button',
+  templateUrl: './black-button.component.html',
+  styleUrls: ['./black-button.component.css']
+})
+export class BlackButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/black-button/black-button.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BlackButtonComponent } from './black-button.component';
+
+
+
+@NgModule({
+  declarations: [BlackButtonComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BlackButtonModule { }
